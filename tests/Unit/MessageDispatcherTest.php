c8bbb8486f0d69f332de58f12a3bac17da994dc9
Jon Cram <webignition@gmail.com>
2021-04-08 09:57:44 +0100

    Add DelayedMessageMiddleware

--- tests/Unit/MessageDispatcherTest.php
+++ tests/Unit/MessageDispatcherTest.php
@@ -6,25 +6,58 @@ namespace webignition\SymfonyMessengerMessageDispatcher\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Stamp\DelayStamp;
 use webignition\SymfonyMessengerMessageDispatcher\MessageDispatcher;
+use webignition\SymfonyMessengerMessageDispatcher\Middleware\DelayedMessageMiddleware;
+use webignition\SymfonyMessengerMessageDispatcher\MiddlewareInterface;
+use webignition\SymfonyMessengerMessageDispatcher\Tests\Model\MessageOne;
 use webignition\SymfonyMessengerMessageDispatcher\Tests\Services\MessageBus;
 
 class MessageDispatcherTest extends TestCase
 {
-    public function testDispatch(): void
+    /**
+     * @dataProvider dispatchDataProvider
+     *
+     * @param MiddlewareInterface[] $middleware
+     */
+    public function testDispatch(array $middleware, object $message, Envelope $expectedEnvelope): void
     {
-        $message = (object) [
-            'key' => 'value',
-        ];
-
         $messageBus = new MessageBus();
 
-        $messageDispatcher = new MessageDispatcher($messageBus);
+        $messageDispatcher = new MessageDispatcher($messageBus, $middleware);
 
         $messageDispatcher->dispatch($message);
 
         $messageBusStack = $messageBus->getStack();
 
-        self::assertEquals(Envelope::wrap($message), $messageBusStack[0]);
+        self::assertEquals($expectedEnvelope, $messageBusStack[0]);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function dispatchDataProvider(): array
+    {
+        return [
+            'no middleware' => [
+                'middleware' => [],
+                'message' => new MessageOne(),
+                'expectedEnvelope' => Envelope::wrap(new MessageOne()),
+            ],
+            'delayed message middleware' => [
+                'middleware' => [
+                    new DelayedMessageMiddleware([
+                        MessageOne::class => 1000,
+                    ])
+                ],
+                'message' => new MessageOne(),
+                'expectedEnvelope' => Envelope::wrap(
+                    new MessageOne(),
+                    [
+                        new DelayStamp(1000)
+                    ]
+                ),
+            ],
+        ];
     }
 }
